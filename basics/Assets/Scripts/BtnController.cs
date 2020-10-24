6d1b571579b503a9141d75eaa8f6bc2512befea5
dnwldus410 <69383333+dnwldus410@users.noreply.github.com>
2020-10-25 02:48:03 +0900

    .

--- basics/Assets/Scripts/BtnController.cs
+++ basics/Assets/Scripts/BtnController.cs
@@ -8,19 +8,32 @@ public class BtnController : MonoBehaviour
     public GameObject stopcv;
    // public GameObject exppcv;
     private bool b_stopcv=false;
-   // private bool b_exppcv = false;
+    public AudioSource btnclickSound;
+    public AudioSource btnbackSound;
+    public AudioSource btnsetSound;
+    // private bool b_exppcv = false;
     public void startBtn()
     {
         SceneManager.LoadScene("GameScene");
     }
     public void restartBtn() //=okbtn
     {
+        btnclickSound.Play();
+
+        PlayerPrefs.DeleteAll();  //기록들 다 사라지고 새로시작
+        SceneManager.LoadScene("GameScene");
+    }
+    public void quit_restartBtn() //=okbtn
+    {
+        btnclickSound.Play();
 
         PlayerPrefs.DeleteAll();  //기록들 다 사라지고 새로시작
         SceneManager.LoadScene("StartScene");
     }
     public void AgainBtn()
     {
+        btnclickSound.Play();
+
         if (b_stopcv)
         {
             stopcv.SetActive(false);
@@ -33,37 +46,26 @@ public class BtnController : MonoBehaviour
     }
     public void OkBtn() //지도화면 ,= quitbtn
     {
+        btnclickSound.Play();
         Application.CancelQuit();
 
 
     }
-    public void exppBtn()
-    {
-    /*
-        if (b_exppcv == false)
-        {
-            exppcv.SetActive(true);
-            
-            b_exppcv = true;
-        }
-        else if (b_exppcv)
-        {
-            exppcv.SetActive(false);
-          
-            b_exppcv = false;
-        }*/
-    }
+
   
     public void StopBtn()
     {
+        
         if (b_stopcv == false)
         {
+            btnclickSound.Play();
             stopcv.SetActive(true);
             Time.timeScale = 0.0f;
             b_stopcv = true;
         }
         else if (b_stopcv)
         {
+            btnbackSound.Play();
             stopcv.SetActive(false);
             if (Time.timeScale == 0.0f)
             {
